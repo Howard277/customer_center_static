441b2a10ddf6d0a74c62c9666065011ee8313691
wuketao <wuketao@houbank.com>
2018-12-05 13:33:36 +0800

    优化

--- src/api/customer.js
+++ src/api/customer.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
+
 export default {
   // 查询全部客户信息
   all () {
-    return axios.get('/api/customer/all')
+    return axios.get('/customer_center_sidecar/customer/all')
   },
   // 保存客户信息
   save (params) {
-    return axios.post('/api/customer/save', params)
+    return axios.post('/customer_center_sidecar/customer/save', params)
   },
   // 删除客户信息
   delete_by_id (params) {
-    return axios.post('/api/customer/delete_by_id', params)
+    return axios.post('/customer_center_sidecar/customer/delete_by_id', params)
   },
   // 通过调节获取分页数据
   page_by_condition (params) {
-    return axios.post('/api/customer/page_by_condition', params)
+    return axios.post('/customer_center_sidecar/customer/page_by_condition', params)
   }
 }
